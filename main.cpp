efe77d75d28cb9dd99bced4037d7816d35f43f01
Florian Roméo <florian.romeo@feragon.net>
2018-09-23 20:25:40 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <clang/Tooling/Tooling.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <iostream>
+
+using namespace clang;
+
+int main(int argc, const char** argv) {
+    return 0;
+}
