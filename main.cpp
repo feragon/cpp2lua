4056ce70210bb492ef35e0884dcf8aaa1de7f6d1
Florian Roméo <florian.romeo@feragon.net>
2018-09-23 21:04:02 +0200

    libtooling test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <clang/Tooling/Tooling.h>
 #include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Frontend/FrontendActions.h>
 #include <iostream>
 
-using namespace clang;
-
 int main(int argc, const char** argv) {
-    return 0;
+    llvm::cl::OptionCategory category("cpp2lua");
+    clang::tooling::CommonOptionsParser op(argc, argv, category);
+    clang::tooling::ClangTool tool(op.getCompilations(), op.getSourcePathList());
+    return tool.run(clang::tooling::newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 }
